ba0789c192dc5a921f8b82fa3564958dfae860a2
oscarpinzonmesi <oscarpinzon2007@gmail.com>
2025-09-10 22:14:18 -0500

    t

--- bridge_bot.py
+++ bridge_bot.py
@@ -37,9 +37,12 @@ def webhook():
 
     return {"ok": True}
 
-@app.route("/")
+# === RUTA HOME (GET) ===
+@app.route("/", methods=["GET"])
 def home():
     return "✅ Bridge Bot activo"
+
+# === MAIN PARA HEROKU ===
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Heroku asigna un puerto dinámico
     app.run(host="0.0.0.0", port=port)
